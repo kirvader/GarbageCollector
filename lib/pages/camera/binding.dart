b9c2e6ad00c32f773f57e5dfcd66e9e01eac1cb1
kirushka <kkondratiu@jacobs-university.de>
2022-11-05 16:38:49 +0100

    renamed camera binding

--- lib/pages/camera/binding.dart
+++ lib/pages/camera/binding.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 import 'controller.dart';
 
-class CameraBinding extends Bindings {
+class CameraPageBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<CameraPageController>(() => CameraPageController());
