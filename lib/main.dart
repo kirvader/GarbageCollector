a0556f0b12d8611857976edc9f899127e2157cd0
kirushka <kkondratiu@jacobs-university.de>
2022-11-05 17:57:18 +0100

    added flutter_map simple implementation

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ ThemeData getTheme() {
 
 void main() {
   runApp(GetMaterialApp(
-    initialRoute: Routes.main,
+    initialRoute: Routes.map,
     theme: getTheme(),
     getPages: AppPages.pages,
     enableLog: true,
--- lib/pages/map/view.dart
+++ lib/pages/map/view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:get/get.dart';
 
 import 'controller.dart';
@@ -8,34 +10,23 @@ class MapPageView extends GetView<MapPageController> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-    // return GetRouterOutlet.builder(
-    //   routerDelegate: Get.rootDelegate,
-    //   builder: (context, delegate, currentRoute) {
-    //     return controller.obx((state) {
-    //       return Scaffold(
-    //           body: GetRouterOutlet(
-    //               initialRoute: Routes.PROFILE, delegate: delegate),
-    //           bottomNavigationBar: BottomBarButtons(
-    //               onTapOnBottomBar: (value) {
-    //                 controller.updateTabIndex(value);
-    //                 switch (value) {
-    //                   case 0:
-    //                     delegate.toNamed(Routes.VOTE,
-    //                         arguments: state?.data.nextQuestion);
-    //                     break;
-    //                   case 1:
-    //                     delegate.toNamed(Routes.PROFILE);
-    //                     break;
-    //                   default:
-    //                 }
-    //               },
-    //               currentIndex: state?.pageIndex ?? DEFAULT_OPENED_PAGE_INDEX));
-    //     },
-    //         onLoading: Container(
-    //             color: BACKGROUND,
-    //             child: const SpinKitDoubleBounce(color: Colors.blue)));
-    //   },
-    // );
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.509364, -0.128928),
+        zoom: 9.2,
+      ),
+      nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+          source: 'OpenStreetMap contributors',
+          onSourceTapped: null,
+        ),
+      ],
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+      ],
+    );
   }
 }
